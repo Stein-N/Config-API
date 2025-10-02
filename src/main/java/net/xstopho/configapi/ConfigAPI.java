b232470f748ca3a39e686170018693290b835d0f
Stein-N <nico-stein-98@sbvl.de>
2025-10-02 15:39:59 +0200

    rename everything

---
+++ src/main/java/net/xstopho/configapi/ConfigAPI.java
@@ -0,0 +1,15 @@
+package net.xstopho.configapi;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConfigAPI implements ModInitializer {
+    public static final String MOD_ID = "config-api";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
