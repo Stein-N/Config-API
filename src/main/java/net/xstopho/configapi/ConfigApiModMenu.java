f0c00e5f4b5a47c4e5e89ca36c78b27f2548cf0e
Stein-N <nico-stein-98@sbvl.de>
2025-10-03 21:40:04 +0200

    add correct ModMenu Implementation

--- src/main/java/net/xstopho/configapi/ConfigApiModMenu.java
+++ src/main/java/net/xstopho/configapi/ConfigApiModMenu.java
@@ -2,11 +2,21 @@ package net.xstopho.configapi;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.xstopho.configapi.api.ConfigRegistry;
 import net.xstopho.configapi.client.gui.ConfigApiScreen;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConfigApiModMenu implements ModMenuApi {
     @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return screen -> new ConfigApiScreen(screen, ConfigApi.MOD_ID);
+    public Map<String, ConfigScreenFactory<?>> getProvidedConfigScreenFactories() {
+        Map<String, ConfigScreenFactory<?>> screenFactoryMap = new HashMap<>();
+
+        ConfigRegistry.getConfigList().forEach(config -> {
+            screenFactoryMap.putIfAbsent(config.getModId(), screen -> new ConfigApiScreen(screen, config.getModId()));
+        });
+
+        return screenFactoryMap;
     }
 }
